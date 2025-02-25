a78eee0b428c9d40f93d9d4f457f9586bac9e0c6
SHANTO <rabbymd55555@gmail.com>
2025-02-25 06:01:47 +0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hi');
+// option-2
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function makeRed(){
+   document.body.style.backgroundColor= 'red';
+}
+
+// option 3 : get element by Id and then set onclick
+const btnMakeBlue = document.getElementById('btn-make-blue');
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue'
+}
+
+
+
+const btnMakePurple =document.getElementById('btn-make-purple');
+console.log(btnMakePurple);
